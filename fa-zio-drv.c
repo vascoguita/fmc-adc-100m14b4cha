0a448cc99a1b4bf8bc48a2612fded57ccc6fc57a
Federico Vaga <federico.vaga@gmail.com>
2012-08-26 19:50:51 +0200

    fa-zio-drv.c: move zio_trigger_abort in fsm-command case
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- fa-zio-drv.c
+++ fa-zio-drv.c
@@ -237,17 +237,6 @@ static int zfad_conf_set(struct device *dev, struct zio_attribute *zattr,
 			reg = &zfad_regs[i];
 			break;
 		case ZFA_CTL_FMS_CMD:
-			/*
-			 * When any command occurs we are ready to start a new
-			 * acquisition, so we must abort any previous one.
-			 * If it is STOP, we abort because we abort
-			 * an acquisition.
-			 * If it is START, we abort because if there was a
-			 * previous start but the acquisition end interrupt
-			 * doesn't occurs, START mean RESTART.
-			 * If it is a clean START, the abort has not effects
-			 */
-			zio_trigger_abort(to_zio_cset(dev));
 			/* Verify the SerDes status */
 			zfa_common_info_get(fa, &zfad_regs[ZFA_STA_SERDES_PLL],
 					    &val);
@@ -262,6 +251,17 @@ static int zfad_conf_set(struct device *dev, struct zio_attribute *zattr,
 				dev_err(dev, "SerDes is not synchronized\n");
 				return -EBUSY;
 			}
+			/*
+			 * When any command occurs we are ready to start a new
+			 * acquisition, so we must abort any previous one.
+			 * If it is STOP, we abort because we abort
+			 * an acquisition.
+			 * If it is START, we abort because if there was a
+			 * previous start but the acquisition end interrupt
+			 * doesn't occurs, START mean RESTART.
+			 * If it is a clean START, the abort has not effects
+			 */
+			zio_trigger_abort(to_zio_cset(dev));
 		default:
 			reg = &zfad_regs[zattr->priv.addr];
 	}
