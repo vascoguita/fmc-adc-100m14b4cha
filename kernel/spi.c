0312c0429f6fe8931156c1d31b0f0d9f215015c0
Alessandro Rubini <rubini@gnudd.com>
2013-06-24 12:47:08 +0200

    bugfixes: kernel/spi: make it work
    Not all changes here are bug fixes, as I clarified a few minor
    details. SPI was not working though, and I let the reader find the
    5 bugfixes in this commit (5 bugs from 4 errors: one error is twin).
    
    (note, I'm not upset with the original author, he already ecplained
    and apologized)
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- kernel/spi.c
+++ kernel/spi.c
@@ -25,8 +25,9 @@
 #define FA_SPI_REG(_off) (FA_SPI_MEM_OFF + _off)
 
 /* SPI control register fields mask */
-#define FA_SPI_CTRL_BUF		0x007F
-#define FA_SPI_CTRL_BUSY	0x0100
+#define FA_SPI_CTRL_CHAR_LEN	0x007F
+#define FA_SPI_CTRL_GO		0x0100 /* go/busy */
+#define FA_SPI_CTRL_BUSY	0x0100 /* go/busy */
 #define FA_SPI_CTRL_Rx_NEG	0x0200
 #define FA_SPI_CTRL_Tx_NEG	0x0400
 #define FA_SPI_CTRL_LSB		0x0800
@@ -37,20 +38,21 @@
 int fa_spi_xfer(struct fa_dev *fa, int cs, int num_bits,
 		uint32_t tx, uint32_t *rx)
 {
-	uint32_t ctrl = 0;
+	uint32_t ctrl;
 	unsigned long j = jiffies + HZ;
 	int err = 0;
 
-	/* Put out value in the T0 register*/
-	fmc_writel(fa->fmc, (tx & (num_bits-1)), FA_SPI_MEM_OFF + FA_SPI_TX(0));
+	/* Put out value (LSB-aligned) in the T0 register (bits 0..31) */
+	fmc_writel(fa->fmc, tx, FA_SPI_MEM_OFF + FA_SPI_TX(0));
 	/* Configure SPI controller */
-	ctrl |= FA_SPI_CTRL_ASS | /* Automatic handle ChipSelect*/
-		num_bits; /* Transfer num_bits bits */
+	ctrl = FA_SPI_CTRL_ASS |	/* Automatic Slave Select*/
+		FA_SPI_CTRL_Tx_NEG |	/* Change on falling edge */
+		num_bits;		/* In CHAR_LEN field */
 	fmc_writel(fa->fmc, ctrl, FA_SPI_REG(FA_SPI_CTRL));
 	/* Set Chip Select */
-	fmc_writel(fa->fmc, (1 << cs), FA_SPI_REG(FA_SPI_CTRL_ASS));
+	fmc_writel(fa->fmc, (1 << cs), FA_SPI_REG(FA_SPI_CS));
 	/* Start transfer */
-	ctrl |= FA_SPI_CTRL_BUSY;
+	ctrl |= FA_SPI_CTRL_GO;
 	fmc_writel(fa->fmc, ctrl, FA_SPI_REG(FA_SPI_CTRL));
 	/* Wait transfer complete */
 	while(fmc_readl(fa->fmc, FA_SPI_REG(FA_SPI_CTRL)) & FA_SPI_CTRL_BUSY) {
@@ -61,7 +63,7 @@ int fa_spi_xfer(struct fa_dev *fa, int cs, int num_bits,
 		}
 	}
 	/* Transfer compleate, read data */
-	*rx = fmc_readl(fa->fmc, FA_SPI_REG(FA_SPI_RX(0))) & (num_bits-1);
+	*rx = fmc_readl(fa->fmc, FA_SPI_REG(FA_SPI_RX(0))),
 	dev_dbg(&fa->fmc->dev, "SPI transfer CS %d, NBIT %d, TX 0x%x RX 0x%x\n",
 		cs, num_bits, tx, *rx);
 out:
@@ -74,7 +76,24 @@ out:
 
 int fa_spi_init(struct fa_dev *fa)
 {
-	/* nothing to do */
+	uint32_t tx, rx;
+	int i;
+
+	/* Divider must be 100, according to firmware guide */
+	fmc_writel(fa->fmc, 100, FA_SPI_REG(FA_SPI_DIV));
+
+	/* Force 2's complement data output (register 1, bit 5) */
+	fa_spi_xfer(fa, FA_SPI_SS_ADC, 16, (1 << 8) | (1 << 5), &rx);
+
+	if (0) {
+		/* Check the current configuration of the ADC chip */
+		for (i = 0; i < 5; i++) {
+			tx = 0x8000 | (i << 8);
+			fa_spi_xfer(fa, FA_SPI_SS_ADC, 16, tx, &rx);
+			dev_dbg(&fa->fmc->dev, "LTC register %02x: 0x%02x\n",
+				i, rx & 0xff);
+		}
+	}
 	return 0;
 }
 
