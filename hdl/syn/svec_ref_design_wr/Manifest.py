cacc6846084fd4eaa1b8267a57bb3889998b9be6
Dimitris Lampridis <dimitris.lampridis@cern.ch>
2018-11-05 10:18:49 +0100

    hdl: add extra synthesis options to hdlmake manifests

--- hdl/syn/svec_ref_design_wr/Manifest.py
+++ hdl/syn/svec_ref_design_wr/Manifest.py
@@ -29,6 +29,9 @@ fetchto="../../ip_cores"
 
 ctrls = ["bank4_64b_32b", "bank5_64b_32b"]
 
-syn_post_project_cmd = "$(TCL_INTERPRETER) " + \
-                       fetchto + "/general-cores/tools/sdb_desc_gen.tcl " + \
-                       syn_tool + " $(PROJECT_FILE)"
+syn_post_project_cmd = (
+    "$(TCL_INTERPRETER) " + \
+    fetchto + "/general-cores/tools/sdb_desc_gen.tcl " + \
+    syn_tool + " $(PROJECT_FILE);" \
+    "$(TCL_INTERPRETER) syn_extra_steps.tcl $(PROJECT_FILE)"
+)
