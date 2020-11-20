d6af1d7dbadc13eca4e7ef9fc9425543045faf8e
Federico Vaga <federico.vaga@cern.ch>
2020-11-20 12:46:29 +0100

    update LICENSES
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- hdl/rtl/fmc_adc_100Ms_core.vhd
+++ hdl/rtl/fmc_adc_100Ms_core.vhd
@@ -1,4 +1,5 @@
 -------------------------------------------------------------------------------
+-- SPDX-License-Identifier: CERN-OHL-W-2.0
 -- Title      : FMC ADC 100Ms/s core
 -- Project    : FMC ADC 100M 14B 4CHA gateware
 -- URL        : http://www.ohwr.org/projects/fmc-adc-100m14b4cha-gw
@@ -11,18 +12,6 @@
 -- Description: FMC ADC 100Ms/s core.
 -------------------------------------------------------------------------------
 -- Copyright (c) 2011-2020 CERN (BE-CO-HT)
--------------------------------------------------------------------------------
--- GNU LESSER GENERAL PUBLIC LICENSE
--------------------------------------------------------------------------------
--- This source file is free software; you can redistribute it and/or modify it
--- under the terms of the GNU Lesser General Public License as published by the
--- Free Software Foundation; either version 2.1 of the License, or (at your
--- option) any later version. This source is distributed in the hope that it
--- will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
--- of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
--- See the GNU Lesser General Public License for more details. You should have
--- received a copy of the GNU Lesser General Public License along with this
--- source; if not, download it from http://www.gnu.org/licenses/lgpl-2.1.html
 -------------------------------------------------------------------------------
 
 library ieee;
